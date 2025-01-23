ffb02c1a6c3efd799448547454c91dea8f107cd8
IlhomjonWeb <ilhomjonfrontend@gmail.com>
2025-01-23 09:41:38 -1100

    added vue jobs project to github

---
+++ src/components/BackButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <!-- Go Back -->
+    <section>
+        <div class="container m-auto py-6 px-6">
+            <RouterLink to="/jobs" class="text-green-500 hover:text-green-600 flex items-center">
+                <i class="fa-solid fa-arrow-left fa-fade mr-2" style="color: #63E6BE; "></i> Back to Job Listings
+            </RouterLink>
+        </div>
+    </section>
+</template>
